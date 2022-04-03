a3b395826fab2b407a2ab7cd04b18de5483d83fa
Ashkan Goharfar <ashkan.goharfar@gmail.com>
2022-04-03 17:12:02 -0400

    Get the image from Photo library and show on UIImageView.

--- FacialRecognitionApp-iOS/ViewController.swift
+++ FacialRecognitionApp-iOS/ViewController.swift
@@ -6,12 +6,63 @@
 //
 
 import UIKit
+import Vision
+import Photos
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UINavigationControllerDelegate, UIImagePickerControllerDelegate {
+    
+    
+    @IBOutlet weak var messageLabel: UILabel!
+    
+    @IBOutlet weak var pictureChosen: UIImageView!
+    
+    
+    let picker = UIImagePickerController()
+    
+    
+    @IBAction func getImage(_ sender: UIButton) {
+        getPhoto()
+    }
+    
+    func getPhoto() {
 
+        picker.allowsEditing = false
+        picker.sourceType = .photoLibrary
+
+        present(picker, animated: true, completion: nil)
+            
+    }
+    
+    func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey : Any]) {
+           if let pickedImage = info[UIImagePickerController.InfoKey.originalImage] as? UIImage {
+               pictureChosen.contentMode = .scaleAspectFit
+               pictureChosen.image = pickedImage
+           }
+    
+           dismiss(animated: true, completion: nil)
+       }
+    
+    
+    func analyzeImage(image: UIImage)
+    {
+        let handler = VNImageRequestHandler(cgImage: image.cgImage!, options: [ : ])
+        messageLabel.text = "Analyzing picture..."
+        let request =
+        VNDetectFaceRectanglesRequest(completionHandler: handleFaceRecognition)
+        try! handler.perform([request])
+    }
+    
+    func handleFaceRecognition(request: VNRequest, error: Error?) {
+        guard let foundFaces = request.results as? [VNFaceObservation] else {
+            fatalError ("Can't find a face in the picture")
+        }
+        messageLabel.text = "Found \(foundFaces.count) faces in the picture"
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        picker.delegate = self
     }
 
 
