04a64a058b3c753da0925d6aac499c14a361d546
Ashkan Goharfar <ashkan.goharfar@gmail.com>
2022-04-03 17:42:19 -0400

    Faces landmark detection was added.

--- FacialRecognitionApp-iOS/ViewController.swift
+++ FacialRecognitionApp-iOS/ViewController.swift
@@ -17,30 +17,84 @@ class ViewController: UIViewController, UINavigationControllerDelegate, UIImageP
     @IBOutlet weak var pictureChosen: UIImageView!
     
     
-    let picker = UIImagePickerController()
-    
-    
     @IBAction func getImage(_ sender: UIButton) {
         getPhoto()
     }
     
     func getPhoto() {
-
-        picker.allowsEditing = false
-        picker.sourceType = .photoLibrary
-
+        let picker = UIImagePickerController()
+        picker.delegate = self
+        picker.sourceType = .savedPhotosAlbum
         present(picker, animated: true, completion: nil)
-            
     }
     
-    func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey : Any]) {
-           if let pickedImage = info[UIImagePickerController.InfoKey.originalImage] as? UIImage {
-               pictureChosen.contentMode = .scaleAspectFit
-               pictureChosen.image = pickedImage
-           }
+    func identifyFacesWithLandmarks(image: UIImage)
+    {
+        let handler = VNImageRequestHandler(cgImage: image.cgImage!, options: [ : ])
+        let request =
+        VNDetectFaceLandmarksRequest(completionHandler: handleFaceLandmarksRecognition)
+        try! handler.perform([request])
+    }
+
+    func handleFaceLandmarksRecognition(request: VNRequest, error: Error?)
+    {
+        guard let foundFaces = request.results as? [VNFaceObservation] else
+        {
+        fatalError ("Problem loading picture to examine faces")
+        }
+
+        for faceRectangle in foundFaces
+        {
+        let landmarkRegions: [VNFaceLandmarkRegion2D] = []
+        drawImage(source: pictureChosen.image!,
+        boundary: faceRectangle.boundingBox, faceLandmarkRegions: landmarkRegions)
+        }
+    }
+
+    func drawImage(source: UIImage, boundary: CGRect, faceLandmarkRegions: [VNFaceLandmarkRegion2D])
+    {
+        UIGraphicsBeginImageContextWithOptions(source.size, false, 1)
+        let context = UIGraphicsGetCurrentContext()!
+        context.translateBy(x: 0, y: source.size.height)
+        context.scaleBy(x: 1.0, y: -1.0)
+        context.setLineJoin(.round)
+        context.setLineCap(.round)
+        context.setShouldAntialias(true)
+        context.setAllowsAntialiasing(true)
+        let rect = CGRect(x: 0, y:0, width: source.size.width, height: source.size.height)
+        context.draw(source.cgImage!, in: rect)
+        //draw rectangles around faces
+        let fillColor = UIColor.systemGreen
+        fillColor.setStroke()
+        context.setLineWidth(10.0)
+        let rectangleWidth = source.size.width * boundary.size.width
+        let rectangleHeight = source.size.height * boundary.size.height
+
+        context.addRect(CGRect(
+        x: boundary.origin.x * source.size.width,
+        y:boundary.origin.y * source.size.height,
+        width: rectangleWidth,
+        height: rectangleHeight))
+
+        context.drawPath(using: CGPathDrawingMode.stroke)
+        let modifiedImage : UIImage = UIGraphicsGetImageFromCurrentImageContext()!
+        UIGraphicsEndImageContext()
+        pictureChosen.image = modifiedImage
+    }
     
-           dismiss(animated: true, completion: nil)
-       }
+    
+    func imagePickerController(_ picker: UIImagePickerController,
+        didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey : Any])
+        {
+        if let gotImage =
+        info[UIImagePickerController.InfoKey.originalImage] as? UIImage
+        {
+        picker.dismiss(animated: true, completion: nil)
+         pictureChosen.image = gotImage
+        analyzeImage(image: gotImage)
+        identifyFacesWithLandmarks(image: gotImage)
+        }
+     }
     
     
     func analyzeImage(image: UIImage)
@@ -62,7 +116,7 @@ class ViewController: UIViewController, UINavigationControllerDelegate, UIImageP
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
-        picker.delegate = self
+//        picker.delegate = self
     }
 
 
